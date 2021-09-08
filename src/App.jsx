7f26232e3df75fe7c927e9cf45644c33c5355a24
Chris <to.chris.lindstrom@gmail.com>
2021-09-08 18:17:44 +0200

    Setting up navbar and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Popular from './pages/Popular';
 
 function App() {
   return (
     <div className="App">
-      start
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Popular} />
+        <Route path="/now-playing" component={Popular} />
+        <Route path="/top-rated" component={Popular} />
+        <Route path="/genres" component={Popular} />
+      </Switch>
     </div>
   )
 }
