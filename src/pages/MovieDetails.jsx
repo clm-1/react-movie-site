53d8a52734a2625e7281d406966a62b69c8741f9
Chris <to.chris.lindstrom@gmail.com>
2021-09-11 18:08:57 +0200

    Person details and listing credits

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -10,6 +10,8 @@ const MovieDetails = () => {
   const { data, isError, error, isLoading } = useQuery(['details', id], () => {
     return getMovie(id);
   });
+
+  // Prefixes for poster and cover img
   const imgPrefix = 'https://image.tmdb.org/t/p/w500';
   const coverImgPrefix = 'https://image.tmdb.org/t/p/original';
 
@@ -17,19 +19,21 @@ const MovieDetails = () => {
   data && console.log(data);
 
   const renderHeader = () => {
+    // Create string of all genres 
     const genreString = data.genres.map(genre => (
       genre.name
     )).join(' / ');
 
+    // Render the movie page header
     return (
       <div className={styles.movieDetailsHeader}>
         <div className={styles.headerOverlay}></div>
-        <img className={styles.coverImg} src={`${coverImgPrefix}${data.backdrop_path}`} alt="" />
+        <img className={styles.coverImg} src={data.backdrop_path && `${coverImgPrefix}${data.backdrop_path}`} alt="" />
         <div className={`${styles.headerInfoWrapper} page-container`}>
           <img src={`${imgPrefix}${data.poster_path}`} alt={`${data.title} poster`} />
           <div className={styles.headerText}>
             <div className={styles.headerTitle}>
-              <h1>{data.title} <span className={styles.releaseYear}>({data.release_date.slice(0, 4)})</span></h1>
+              <h1>{data.title} <span className={styles.releaseYear}>{ data.release_date && `(${data.release_date.slice(0, 4)})` }</span></h1>
             </div>
             <div className={styles.headerInfo}>
               <p>{genreString}</p>
@@ -45,7 +49,7 @@ const MovieDetails = () => {
             </div>
             <div className={styles.headerDirector}>
               <h2>Director:</h2>
-              <p>{data.credits.crew.find(person => person.job === 'Director').name}</p>
+              <p>{data.credits.crew.find(person => person.job === 'Director') ? data.credits.crew.find(person => person.job === 'Director').name : 'Unknown'}</p>
             </div>
           </div>
         </div>
@@ -60,11 +64,11 @@ const MovieDetails = () => {
       <div className="page-container">
         <h2>Cast:</h2>
         <div className={styles.actorsWrapper}>
-          {data && data.credits.cast.slice(0, 10).map(actor => {
+          {data && data.credits.cast.slice(0, 10).map((actor, i) => {
             if (actor.profile_path !== null) {
               return (
                 <div
-                  key={actor.id}
+                  key={i}
                   className={styles.actorCard}
                   onClick={() => history.push(`/people/${actor.id}`)}>
                   <img src={`${imgPrefix}${actor.profile_path}`}></img>
@@ -78,8 +82,8 @@ const MovieDetails = () => {
           <>
             <h2>Full Cast:</h2>
             <div className={styles.fullCast}>
-              {data && data.credits.cast.slice(10).map(actor =>
-                <p key={actor.id} onClick={() => history.push(`/person/${actor.id}`)}>{actor.name}</p>)}
+              {data && data.credits.cast.slice(10).map((actor, i) =>
+                <p key={i} onClick={() => history.push(`/people/${actor.id}`)}>{actor.name}</p>)}
             </div>
           </>}
       </div>
