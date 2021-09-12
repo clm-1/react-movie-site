e691107a80f500c72addc50e091e4b7e9af02895
Chris <to.chris.lindstrom@gmail.com>
2021-09-12 11:16:56 +0200

    Made next and previous btns into own component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { useUrlSearchParams } from 'use-url-search-params';
-import MovieCard from '../components/MovieCard';
 import { getMovies, getMoviesByGenre } from '../services/MovieAPI';
-import styles from '../css/MovieList.module.css';
+import MovieCard from '../components/MovieCard';
 import PageNotFound from './PageNotFound';
+import styles from '../css/MovieList.module.css';
+import NextPrevBtns from './NextPrevBtns';
 
 const MovieList = ({ type = null, genre = null }) => {
+  // Using url search params to keep track of current page
   const [params, setParams] = useUrlSearchParams({ page: 1 });
-  const history = useHistory();
   // React Query gets data based on what type is sent into the component from the parent page (Popular, NowPlaying, TopRated)
   const { data, error, isError, isLoading } = useQuery([type, genre, params.page], () => {
     if (genre) return getMoviesByGenre(genre, params.page);
@@ -17,46 +18,36 @@ const MovieList = ({ type = null, genre = null }) => {
     return getMovies(type, params.page);
   });
 
+  const history = useHistory();
+
   useEffect(() => {
+    // Check page in params
+    // If page is NaN or below 1, set page to 1
     if (isNaN(+params.page) || params.page <= 0) setParams({ page: 1 });
   }, [params.page])
 
   data && console.log(data);
 
-  const renderButtons = () => {
-    return (
-      <div className={styles.pageNumberWrapper}>
-        <button disabled={params.page === '1'} onClick={() => {
-          if (params.page !== 1) {
-            setParams({ page: +params.page - 1 });
-          }
-        }}>{'<'}</button>
-        <h2>Page: {params.page} / {data.total_pages && data.total_pages}</h2>
-        <button disabled={params.page >= data.total_pages} onClick={() => {
-          if (params.page >= data.total_pages) {
-            return;
-          } else {
-            setParams({ page: +params.page + 1 })
-          }
-        }}>{'>'}</button>
-      </div>
-    )
-  }
-
   return (
     <div>
       {isLoading && <p>Loading...</p>}
-      {isError && <p>Something went wrong...</p>}
+      {isError && <PageNotFound />}
+
+      {/* Check if we have results and that num of results are above 0 */}
+      {/* Render the buttons at top and bottom of list */}
       {data && data.results.length > 0 &&
         <>
-          {renderButtons()}
+          <NextPrevBtns page={params.page} setParams={setParams} totalPages={data.total_pages}/>
           <div className={styles.listWrapper}>
             {data.results.map((movie, i) => (
               <MovieCard key={i} movie={movie} />
             ))}
           </div>
-          {renderButtons()}
+          <NextPrevBtns page={params.page} setParams={setParams} totalPages={data.total_pages}/>
         </>}
+
+      {/* If page does not exists the array with results will be empty */}
+      {/* Page not found component added */}
       {data && data.results.length < 1 &&
         <PageNotFound />
       }
---
+++ src/components/NextPrevBtns.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '../css/NextPrevBtns.module.css';
+
+// Render next/previous-buttons
+// Check if current page is 1 (should not go back further in that case)
+// Check if current page === total_pages of result and disable "next" button if that is the case
+const NextPrevBtns = ({ page, totalPages, setParams }) => {
+  return (
+    <div className={styles.pageNumberWrapper}>
+      <button disabled={page === '1'} onClick={() => {
+        if (+page !== 1) {
+          setParams({ page: +page - 1 });
+        }
+      }}>{'<'}</button>
+      <h2>Page: {page} / {totalPages && totalPages}</h2>
+      <button disabled={+page >= totalPages} onClick={() => {
+        if (+page >= totalPages) {
+          return;
+        } else {
+          setParams({ page: +page + 1 })
+        }
+      }}>{'>'}</button>
+    </div>
+  )
+}
+
+export default NextPrevBtns
